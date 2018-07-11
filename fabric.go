76b6c16a35c5cddff3917e735e67ce461571d220
Alejandro Escobar <jaescobar.cell@gmail.com>
2018-07-11 14:28:53 -0700

    additional fixes

--- fabric.go
+++ fabric.go
@@ -3,12 +3,6 @@
 
 package gomaasapi
 
-import (
-	"github.com/juju/errors"
-	"github.com/juju/schema"
-	"github.com/juju/version"
-)
-
 // Fabric represents a set of interconnected VLANs that are capable of mutual
 // communication. A Fabric can be thought of as a logical grouping in which
 // VLANs can be considered unique.
@@ -22,90 +16,9 @@ type fabric struct {
 
 	ResourceURI string `json:"resource_uri,omitempty"`
 
-	ID        int
-	Name      string
-	ClassType string
+	ID        int    `json:"ID,omitempty"`
+	Name      string `json:"Name,omitempty"`
+	ClassType string `json:"class_type,omitempty"`
 
-	VLANs []*vlan
-}
-
-func readFabrics(controllerVersion version.Number, source interface{}) ([]*fabric, error) {
-	checker := schema.List(schema.StringMap(schema.Any()))
-	coerced, err := checker.Coerce(source, nil)
-	if err != nil {
-		return nil, errors.Annotatef(err, "Fabric base schema check failed")
-	}
-	valid := coerced.([]interface{})
-
-	var deserialisationVersion version.Number
-	for v := range fabricDeserializationFuncs {
-		if v.Compare(deserialisationVersion) > 0 && v.Compare(controllerVersion) <= 0 {
-			deserialisationVersion = v
-		}
-	}
-	if deserialisationVersion == version.Zero {
-		return nil, errors.Errorf("no Fabric read func for version %s", controllerVersion)
-	}
-	readFunc := fabricDeserializationFuncs[deserialisationVersion]
-	return readFabricList(valid, readFunc)
-}
-
-// readFabricList expects the Values of the sourceList to be string maps.
-func readFabricList(sourceList []interface{}, readFunc fabricDeserializationFunc) ([]*fabric, error) {
-	result := make([]*fabric, 0, len(sourceList))
-	for i, value := range sourceList {
-		source, ok := value.(map[string]interface{})
-		if !ok {
-			return nil, errors.Errorf("unexpected value for Fabric %d, %T", i, value)
-		}
-		fabric, err := readFunc(source)
-		if err != nil {
-			return nil, errors.Annotatef(err, "Fabric %d", i)
-		}
-		result = append(result, fabric)
-	}
-	return result, nil
-}
-
-type fabricDeserializationFunc func(map[string]interface{}) (*fabric, error)
-
-var fabricDeserializationFuncs = map[version.Number]fabricDeserializationFunc{
-	twoDotOh: fabric_2_0,
-}
-
-func fabric_2_0(source map[string]interface{}) (*fabric, error) {
-	fields := schema.Fields{
-		"resource_uri": schema.String(),
-		"ID":           schema.ForceInt(),
-		"Name":         schema.String(),
-		"class_type":   schema.OneOf(schema.Nil(""), schema.String()),
-		"VLANs":        schema.List(schema.StringMap(schema.Any())),
-	}
-	checker := schema.FieldMap(fields, nil) // no defaults
-	coerced, err := checker.Coerce(source, nil)
-	if err != nil {
-		return nil, errors.Annotatef(err, "Fabric 2.0 schema check failed")
-	}
-	valid := coerced.(map[string]interface{})
-	// From here we know that the map returned from the schema coercion
-	// contains fields of the right type.
-
-	vlans, err := readVLANList(valid["VLANs"].([]interface{}), vlan_2_0)
-	if err != nil {
-		return nil, errors.Trace(err)
-	}
-
-	// Since the class_type is optional, we use the two part cast assignment. If
-	// the cast fails, then we get the default value we care about, which is the
-	// empty string.
-	classType, _ := valid["class_type"].(string)
-
-	result := &fabric{
-		ResourceURI: valid["resource_uri"].(string),
-		ID:          valid["ID"].(int),
-		Name:        valid["Name"].(string),
-		ClassType:   classType,
-		VLANs:       vlans,
-	}
-	return result, nil
+	VLANs []*vlan `json:"VLANs,omitempty"`
 }
--- staticroute.go
+++ staticroute.go
@@ -3,12 +3,6 @@
 
 package gomaasapi
 
-import (
-	"github.com/juju/errors"
-	"github.com/juju/schema"
-	"github.com/juju/version"
-)
-
 // StaticRoute defines an explicit route that users have requested to be added
 // for a given Subnet.
 type staticRoute struct {
@@ -28,89 +22,4 @@ type staticRoute struct {
 	// also a more concrete route for 10.0/16 that should take precedence if it
 	// applies.) Metric should be a non-negative integer.
 	Metric int
-}
-
-func readStaticRoutes(controllerVersion version.Number, source interface{}) ([]*staticRoute, error) {
-	checker := schema.List(schema.StringMap(schema.Any()))
-	coerced, err := checker.Coerce(source, nil)
-	if err != nil {
-		return nil, errors.Annotatef(err, "static-route base schema check failed")
-	}
-	valid := coerced.([]interface{})
-
-	var deserialisationVersion version.Number
-	for v := range staticRouteDeserializationFuncs {
-		if v.Compare(deserialisationVersion) > 0 && v.Compare(controllerVersion) <= 0 {
-			deserialisationVersion = v
-		}
-	}
-	if deserialisationVersion == version.Zero {
-		return nil, errors.Errorf("no static-route read func for version %s", controllerVersion)
-	}
-	readFunc := staticRouteDeserializationFuncs[deserialisationVersion]
-	return readStaticRouteList(valid, readFunc)
-}
-
-// readStaticRouteList expects the Values of the sourceList to be string maps.
-func readStaticRouteList(sourceList []interface{}, readFunc staticRouteDeserializationFunc) ([]*staticRoute, error) {
-	result := make([]*staticRoute, 0, len(sourceList))
-	for i, value := range sourceList {
-		source, ok := value.(map[string]interface{})
-		if !ok {
-			return nil, errors.Errorf("unexpected value for static-route %d, %T", i, value)
-		}
-		staticRoute, err := readFunc(source)
-		if err != nil {
-			return nil, errors.Annotatef(err, "static-route %d", i)
-		}
-		result = append(result, staticRoute)
-	}
-	return result, nil
-}
-
-type staticRouteDeserializationFunc func(map[string]interface{}) (*staticRoute, error)
-
-var staticRouteDeserializationFuncs = map[version.Number]staticRouteDeserializationFunc{
-	twoDotOh: staticRoute_2_0,
-}
-
-func staticRoute_2_0(source map[string]interface{}) (*staticRoute, error) {
-	fields := schema.Fields{
-		"resource_uri": schema.String(),
-		"ID":           schema.ForceInt(),
-		"Source":       schema.StringMap(schema.Any()),
-		"Destination":  schema.StringMap(schema.Any()),
-		"gateway_ip":   schema.String(),
-		"Metric":       schema.ForceInt(),
-	}
-	checker := schema.FieldMap(fields, nil) // no defaults
-	coerced, err := checker.Coerce(source, nil)
-	if err != nil {
-		return nil, errors.Annotatef(err, "static-route 2.0 schema check failed")
-	}
-	valid := coerced.(map[string]interface{})
-	// From here we know that the map returned from the schema coercion
-	// contains fields of the right type.
-
-	// readSubnetList takes a list of interfaces. We happen to have 2 Subnets
-	// to parse, that are in different keys, but we might as well wrap them up
-	// together and pass them in.
-	subnets, err := readSubnetList([]interface{}{valid["Source"], valid["Destination"]}, subnet_2_0)
-	if err != nil {
-		return nil, errors.Trace(err)
-	}
-	if len(subnets) != 2 {
-		// how could we get here?
-		return nil, errors.Errorf("Subnets somehow parsed into the wrong number of items (expected 2): %d", len(subnets))
-	}
-
-	result := &staticRoute{
-		ResourceURI: valid["resource_uri"].(string),
-		ID:          valid["ID"].(int),
-		GatewayIP:   valid["gateway_ip"].(string),
-		Metric:      valid["Metric"].(int),
-		Source:      subnets[0],
-		Destination: subnets[1],
-	}
-	return result, nil
 }
